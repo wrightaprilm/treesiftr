79bbd0adf926725210769795710af37d3a88a753
April Wright <wright.aprilm@gmail.com>
2018-07-25 17:05:51 -0500

    adding last test

---
+++ tests/testthat/test_vis_vec.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(ggtree)
+library(phangorn)
+library(Biostrings)
+test_that("Test generate_tree_vis(data_frame, alignment, tree, pscore, lscore)", {
+  data(bears)
+  aln_path = "testdata/bears_fasta.fa"
+  sample_df = generate_sliding(bears, 1, 10, 1)
+  vec <- generate_tree_vis(sample_df = sample_df, alignment = aln_path,
+                           tree = tree, phy_mat = bears)
+    testthat::expect_equal(length(vec), 10)
+    expect_equal(length(vec[[1]]$layers), 5)
+    expect_is(vec[[1]]$labels$title, "numeric")
+})
