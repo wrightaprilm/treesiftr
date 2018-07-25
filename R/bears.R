86694d7ef2bda27e1d88216ce9ac3d1bdb7ca74d
April Wright <wright.aprilm@gmail.com>
2018-07-25 17:02:41 -0500

    data doc

---
+++ R/bears.R
@@ -0,0 +1,11 @@
+#' Dataset of Morphological character for extant and extinct bears.
+#'
+#' A dataset containing 18 taxa and 62 characters for bears.
+#' @format
+#' \describe{
+#' \item{bears}{A fasta dataset frame with 53940 rows and 10 variables}
+#' \item{tree}{A phylo object of a phylogeny}
+#' \item{phy_mat}{Bears dataset formatted as a phyDat object}
+#' }
+#' @source \url{https://github.com/revbayes/revbayes_tutorial/tree/master/RB_TotalEvidenceDating_FBD_Tutorial/data}
+"bears"
