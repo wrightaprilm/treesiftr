c427b483004ffe8d4a796fcd11aad4b0fc0a4acc
April Wright <wright.aprilm@gmail.com>
2019-07-13 14:23:53 -0500

    remove site argument

--- R/generate_tree_vec.R
+++ R/generate_tree_vec.R
@@ -16,7 +16,7 @@ generate_tree_vec <- function(phy_mat, start, stop, tree){
   small_mat <- subset(phy_mat, select=chars[1]:chars[2],
                       site.pattern=FALSE)
   tr <- ape::multi2di(phangorn::optim.parsimony(tree=tree, data = small_mat))
-  p_score <- phangorn::fitch(tr, small_mat, site = pscore)
+  p_score <- phangorn::fitch(tr, small_mat)
   tr$pars2 <- p_score
   p_tr <- phangorn::acctran(tr, data = small_mat)
   fit <- phangorn::pml(p_tr, data = small_mat)
